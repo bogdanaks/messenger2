66f665aeb00846ee6e897f93f173b56f988703b2
bogdanaks <bogdanaks@bk.ru>
2020-09-04 12:49:31 +0300

    Create clear pages

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from './styles.module.scss'
+
 export const Main = () => {
     return (
-        <div>
+        <div className={styles.mainWrapper}>
             <h1>Main page</h1>
         </div>
     )
