5b249f485cf219ad6d31351924eb0f542f938020
bogdanaks <bogdanaks@bk.ru>
2020-09-04 19:56:25 +0300

    Create component

---
+++ client/src/components/Dialog/Dialog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const Dialog = () => {
+    return (
+        <div className={styles.dialogWrapper}>
+            <h1>Dialog</h1>
+        </div>
+    )
+}
