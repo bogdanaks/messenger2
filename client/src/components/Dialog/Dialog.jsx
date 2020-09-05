961767956a502b6ec59fb8516337dbd0a9e4591d
bogdanaks <bogdanaks@bk.ru>
2020-09-05 10:38:39 +0300

    Base style component

--- client/src/components/Dialog/Dialog.jsx
+++ client/src/components/Dialog/Dialog.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGrin } from '@fortawesome/free-regular-svg-icons'
 
 import styles from './styles.module.scss'
+
+import { Message } from '../Message/Message'
 
 export const Dialog = () => {
     return (
         <div className={styles.dialogWrapper}>
-            <h1>Dialog</h1>
+            <div className={styles.header}>
+                <div className={styles.userInfo}>
+                    <h5>Name</h5>
+                    <span>4 members</span>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.content__messages}>
+                    <Message />
+                    <Message me />
+                    <Message />
+                </div>
+                <div className={styles.content__footer}>
+                    <div className={styles.input}>
+                        <input type="text" placeholder="Type text..." />
+                    </div>
+                    <div className={styles.icons}>
+                        <FontAwesomeIcon icon={faGrin} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
