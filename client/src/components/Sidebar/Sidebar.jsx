ff03916b057176843ee21a800689dda3d7b6aed2
bogdanaks <bogdanaks@bk.ru>
2020-09-05 11:25:25 +0300

    Add btn

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faComments } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare } from '@fortawesome/free-regular-svg-icons'
 
 import styles from './styles.module.scss'
 
@@ -14,6 +15,10 @@ export const Sidebar = () => {
                 <li className={styles.active}>
                     <FontAwesomeIcon icon={faComments} />
                     <span>Chats</span>
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faPlusSquare} />
+                    <span>New</span>
                 </li>
             </ul>
         </div>
