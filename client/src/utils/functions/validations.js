b59730033cd3d2dca4796025866c19ad7179ae27
bogdanaks <bogdanaks@bk.ru>
2020-09-06 22:10:02 +0300

    Add base validate rules

--- client/src/utils/functions/validations.js
+++ client/src/utils/functions/validations.js
@@ -2,8 +2,15 @@ export const validations = (values) => {
     let errors = {}
     if (!values.email) {
         errors.email = 'Email address is required'
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    }
+    if (!/\S+@\S+\.\S+/.test(values.email)) {
         errors.email = 'Email address is invalid'
+    }
+    if (!values.password) {
+        errors.password = 'Password is required'
+    }
+    if (values.password && values.password.length < 6) {
+        errors.password = 'Password at least 6 characters'
     }
     return errors
 }
