f1a9f669683fad8ce22f23f064dedbede235707e
bogdanaks <bogdanaks@bk.ru>
2020-09-06 09:38:00 +0300

    Del import component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Main, Auth } from './pages'
-import { Sidebar } from './components/Sidebar/Sidebar'
 
 export const App = () => {
     return (
         <div className="App">
             <Router>
-                <Sidebar />
                 <Switch>
                     <Route path="/" component={Main} exact />
                     <Route path="/auth" component={Auth} exact />
