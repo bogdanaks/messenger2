ce6c93a1b44683387dfcba9eefc04b81d673bdca
bogdanaks <bogdanaks@bk.ru>
2020-09-06 22:20:41 +0300

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import { Main, Auth } from './pages'
+import { Main, Login, Register } from './pages'
 
 export const App = () => {
     return (
@@ -9,7 +9,8 @@ export const App = () => {
             <Router>
                 <Switch>
                     <Route path="/" component={Main} exact />
-                    <Route path="/auth" component={Auth} exact />
+                    <Route path="/login" component={Login} exact />
+                    <Route path="/register" component={Register} exact />
                 </Switch>
             </Router>
         </div>
