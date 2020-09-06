bbc9181f2ff71d0eeeba1e15d0c18fd631c38525
bogdanaks <bogdanaks@bk.ru>
2020-09-06 09:38:40 +0300

    Create component

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import styles from './styles.module.scss'
+
 export const Auth = () => {
     return (
-        <div>
-            <h1>Auth page</h1>
+        <div className={styles.authWrapper}>
+            <form className={styles.form}>
+                <div className={styles.inputForm}>
+                    <input type="text" placeholder="E-mail" />
+                </div>
+                <div className={styles.inputForm}>
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button type="submit">Register</button>
+            </form>
         </div>
     )
 }
