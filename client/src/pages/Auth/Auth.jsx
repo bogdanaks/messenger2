8894197a1712c8109b1af93fd01bb395b7933919
bogdanaks <bogdanaks@bk.ru>
2020-09-06 22:10:36 +0300

    Add  error span

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -16,11 +16,12 @@ export const Auth = () => {
                 <div className={[styles.inputForm, errors.email && styles.errorInput].join(' ')}>
                     <input
                         type="text"
-                        placeholder="E-mail"
+                        placeholder="E-Mail"
                         name="email"
                         onChange={handleChange}
                         value={values.email || ''}
                     />
+                    {errors.email && <span className={styles.errorText}>{errors.email}</span>}
                 </div>
                 <div className={[styles.inputForm, errors.password && styles.errorInput].join(' ')}>
                     <input
@@ -30,6 +31,7 @@ export const Auth = () => {
                         onChange={handleChange}
                         value={values.password || ''}
                     />
+                    {errors.password && <span className={styles.errorText}>{errors.password}</span>}
                 </div>
                 <button type="submit">Register</button>
             </form>
