5b591b1c1879cd018eb7aecf821f605d11c309f9
bogdanaks <bogdanaks@bk.ru>
2020-09-06 09:38:24 +0300

    Fix component

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -5,9 +5,12 @@ import styles from './styles.module.scss'
 export const Message = ({ me }) => {
     return (
         <div className={[styles.msgWrapper, me && styles.me].join(' ')}>
-            <span className={styles.msg}>
-                Как будут заканчивать, напиши, креветки закину! Окей, я понял тебя.
-            </span>
+            <div className={styles.msgBlock}>
+                <h5 className={styles.name}>Name</h5>
+                <span className={styles.msg}>
+                    Как будут заканчивать, напиши, креветки закину! Окей, я понял тебя.
+                </span>
+            </div>
             <span className={styles.data}>18:55</span>
         </div>
     )
