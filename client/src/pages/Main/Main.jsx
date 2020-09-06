ec21384636c2d6aba043a557753b2385920842c2
bogdanaks <bogdanaks@bk.ru>
2020-09-06 09:39:07 +0300

    Import component Sidebar

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 import styles from './styles.module.scss'
 
+import { Sidebar } from '../../components/Sidebar/Sidebar'
 import { DialogsList } from '../../components/DialogsList/DialogsList'
 import { Dialog } from '../../components/Dialog/Dialog'
 
 export const Main = () => {
     return (
-        <div className={styles.mainWrapper}>
-            <DialogsList />
-            <Dialog />
-        </div>
+        <>
+            <Sidebar />
+            <div className={styles.mainWrapper}>
+                <DialogsList />
+                <Dialog />
+            </div>
+        </>
     )
 }
