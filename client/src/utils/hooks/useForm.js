2e6cd0dfc806d91c5dafc61d89f0282178cf2d5f
bogdanaks <bogdanaks@bk.ru>
2020-09-06 18:04:27 +0300

    Create hook useForm

---
+++ client/src/utils/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+
+export const useForm = (callback, validations) => {
+    const [values, setValues] = useState({})
+    const [errors, setErrors] = useState({})
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && isSubmitting) {
+            callback()
+        }
+        // eslint-disable-next-line
+    }, [errors])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setIsSubmitting(true)
+        setErrors(validations(values))
+    }
+    const handleChange = (e) => {
+        e.persist()
+        setValues((prevValue) => ({ ...prevValue, [e.target.name]: e.target.value }))
+    }
+    return {
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+    }
+}
