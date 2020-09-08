18a81098aaba5e299d61da2195977a4b214ae075
bogdanaks <bogdanaks@bk.ru>
2020-09-08 14:59:52 +0300

    Import reducer userReducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import { appReducer } from './appReducer'
+import { userReducer } from './userReducer'
 
 export const rootReducer = combineReducers({
     app: appReducer,
+    user: userReducer,
 })
