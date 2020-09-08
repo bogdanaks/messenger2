ca024bec54ab632415929e8afd54ed004e149b23
bogdanaks <bogdanaks@bk.ru>
2020-09-08 14:26:54 +0300

    Include user routes

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+//routes
+app.use('/api/user', require('./routes/api/user.routes'))
+
 //start server
 async function start() {
     try {
