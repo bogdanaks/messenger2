0897ff089ad84abe29288ecf7c4f553803e77829
bogdanaks <bogdanaks@bk.ru>
2020-09-08 15:00:09 +0300

    Create userReducer

---
+++ client/src/redux/reducers/userReducer.js
@@ -0,0 +1,14 @@
+import { SET_USER } from '../actions/types'
+
+const initialState = {
+    user: {},
+}
+
+export const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, user: action.payload }
+        default:
+            return state
+    }
+}
