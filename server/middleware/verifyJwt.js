8cb5434ec5c67146e1d005d53f9118a55c3c7ab6
bogdanaks <bogdanaks@bk.ru>
2020-09-09 18:46:30 +0300

    Change req

--- server/middleware/verifyJwt.js
+++ server/middleware/verifyJwt.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
         if (!token) return res.status(403).send({ message: 'No token provided!' })
         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
             if (err) return res.status(403).send({ message: 'Forbidden! Invalid token' })
-            req.userId = decoded.id
+            req.id = decoded.id
             next()
         })
     } else {
