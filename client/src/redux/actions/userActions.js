2c9ce144e9e726d3b84a057abc399e317c7b7564
bogdanaks <bogdanaks@bk.ru>
2020-09-09 18:48:04 +0300

    Dispatch alert if has errors

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,17 +1,19 @@
 import { SET_USER } from './types'
 
 import api from '../../utils/functions/axios'
+import { setAlert, clearAlert } from './appActions'
 
 export function registerUser(name, password) {
     return async (dispatch) => {
         try {
-            const res = await api.post('/api/user/register', {
-                name,
-                password,
-            })
-            dispatch({ type: SET_USER, payload: res.data })
+            const res = await api.post('/api/users', { name, password })
+            localStorage.setItem('token', res.data.token)
+            dispatch({ type: SET_USER, payload: { _id: res.data._id, name: res.data.name } })
         } catch (error) {
-            alert('Error')
+            dispatch(setAlert(error.response.status, error.response.data.message))
+            setTimeout(() => {
+                dispatch(clearAlert())
+            }, 2000)
         }
     }
 }
