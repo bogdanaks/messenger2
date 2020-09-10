b32c0207d8ac9c6390422236b549dce20e80656d
bogdanaks <bogdanaks@bk.ru>
2020-09-10 18:55:02 +0300

    Change routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Main, Login, Register } from './pages'
+import { PrivateRoute } from './utils/helpers/PrivateRoute'
+import { IsAuthRoute } from './utils/helpers/IsAuthRoute'
 
 export const App = () => {
     return (
         <div className="App">
             <Router>
                 <Switch>
-                    <Route path="/" component={Main} exact />
-                    <Route path="/login" component={Login} exact />
-                    <Route path="/register" component={Register} exact />
+                    <PrivateRoute path="/" component={Main} exact />
+                    <IsAuthRoute path="/login" component={Login} exact />
+                    <IsAuthRoute path="/register" component={Register} exact />
+                    <Route path="/users" component={Main} />
                 </Switch>
             </Router>
         </div>
