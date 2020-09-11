da42026d4d11e1a34ff7a58b3b2cd44d78c1e92c
bogdanaks <bogdanaks@bk.ru>
2020-09-11 11:32:53 +0300

    Rename types alert and add types for modal

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,4 +1,7 @@
-export const SET_ALERT = 'ALERT/SET_ALERT'
-export const CLEAR_ALERT = 'ALERT/CLEAR_ALERT'
+export const SHOW_ALERT = 'ALERT/SHOW_ALERT'
+export const HIDE_ALERT = 'ALERT/HIDE_ALERT'
+
+export const SHOW_MODAL = 'MODAL/SHOW_MODAL'
+export const HIDE_MODAL = 'MODAL/HIDE_MODAL'
 
 export const SET_USER = 'USER/SET_USER'
