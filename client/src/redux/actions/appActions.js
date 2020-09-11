7a81a4b6f998f027de8144f9fd01d7cbaa2d767f
bogdanaks <bogdanaks@bk.ru>
2020-09-11 11:33:11 +0300

    Rename types alert and add actions for modal

--- client/src/redux/actions/appActions.js
+++ client/src/redux/actions/appActions.js
@@ -1,11 +1,20 @@
-import { SET_ALERT, CLEAR_ALERT } from './types'
+import { SHOW_ALERT, HIDE_ALERT, SHOW_MODAL, HIDE_MODAL } from './types'
 
-export function setAlert(code, message) {
+export function showAlert(code, message) {
     return {
-        type: SET_ALERT,
+        type: SHOW_ALERT,
         payload: { code, message },
     }
 }
-export function clearAlert() {
-    return { type: CLEAR_ALERT }
+export function hideAlert() {
+    return { type: HIDE_ALERT }
+}
+export function showModal(code, message) {
+    return {
+        type: SHOW_MODAL,
+        payload: { code, message },
+    }
+}
+export function hideModal() {
+    return { type: HIDE_MODAL }
 }
