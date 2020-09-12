65cd7e5c15b620afb4bca6c09eb90228068a2c73
bogdanaks <bogdanaks@bk.ru>
2020-09-12 16:14:00 +0300

    Add btn exit methods

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faComments } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import { faPlusSquare } from '@fortawesome/free-regular-svg-icons'
 import styles from './styles.module.scss'
 
@@ -10,15 +11,17 @@ import { showModal } from '../../redux/actions/appActions'
 
 export const Sidebar = () => {
     const dispatch = useDispatch()
+    const history = useHistory()
     const handleBtnNewChat = () => {
         dispatch(showModal('New Chat', 'Enter a name for the new chat'))
+    }
+    const handleBtnExit = () => {
+        localStorage.removeItem('user')
+        history.push('/login')
     }
     return (
         <div className={styles.sidebarWrapper}>
             <ul>
-                <li>
-                    <FontAwesomeIcon icon={faBars} />
-                </li>
                 <li className={styles.active}>
                     <FontAwesomeIcon icon={faComments} />
                     <span>Chats</span>
@@ -26,6 +29,10 @@ export const Sidebar = () => {
                 <li onClick={handleBtnNewChat}>
                     <FontAwesomeIcon icon={faPlusSquare} />
                     <span>New</span>
+                </li>
+                <li className={styles.exitBtn} onClick={handleBtnExit}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                    <span>Exit</span>
                 </li>
             </ul>
         </div>
