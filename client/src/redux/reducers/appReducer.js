736ba8465fdc1cc737800b92fa1e7b428945c383
bogdanaks <bogdanaks@bk.ru>
2020-09-12 15:48:45 +0300

    Add modal reducers

--- client/src/redux/reducers/appReducer.js
+++ client/src/redux/reducers/appReducer.js
@@ -1,9 +1,14 @@
-import { SHOW_ALERT, HIDE_ALERT } from '../actions/types'
+import { SHOW_ALERT, HIDE_ALERT, SHOW_MODAL, HIDE_MODAL } from '../actions/types'
 
 const initialState = {
     alert: {
         code: 0,
         message: '',
+    },
+    modal: {
+        visible: false,
+        header: '',
+        text: '',
     },
 }
 
@@ -13,6 +18,10 @@ export const appReducer = (state = initialState, action) => {
             return { ...state, alert: { ...action.payload } }
         case HIDE_ALERT:
             return { ...state, alert: { ...initialState.alert } }
+        case SHOW_MODAL:
+            return { ...state, modal: { ...action.payload } }
+        case HIDE_MODAL:
+            return { ...state, modal: { ...initialState.modal } }
         default:
             return state
     }
