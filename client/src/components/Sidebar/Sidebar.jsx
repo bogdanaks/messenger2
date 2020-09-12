3f09d0b0c29f931821839aca2ff26cfa7012fe91
bogdanaks <bogdanaks@bk.ru>
2020-09-12 15:47:57 +0300

    Add method for show modal

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faComments } from '@fortawesome/free-solid-svg-icons'
 import { faPlusSquare } from '@fortawesome/free-regular-svg-icons'
-
 import styles from './styles.module.scss'
 
+import { showModal } from '../../redux/actions/appActions'
+
 export const Sidebar = () => {
+    const dispatch = useDispatch()
+    const handleBtnNewChat = () => {
+        dispatch(showModal('New Chat', 'Enter a name for the new chat'))
+    }
     return (
         <div className={styles.sidebarWrapper}>
             <ul>
@@ -16,7 +23,7 @@ export const Sidebar = () => {
                     <FontAwesomeIcon icon={faComments} />
                     <span>Chats</span>
                 </li>
-                <li>
+                <li onClick={handleBtnNewChat}>
                     <FontAwesomeIcon icon={faPlusSquare} />
                     <span>New</span>
                 </li>
