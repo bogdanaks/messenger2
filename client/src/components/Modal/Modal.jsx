553cff98ac0a11bda2ec8eb04733e113bcb1c3d9
bogdanaks <bogdanaks@bk.ru>
2020-09-13 13:14:39 +0300

    Add history and set in dispatch

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 
 import styles from './styles.module.scss'
 
 import { hideModal } from '../../redux/actions/appActions'
+import { createChat } from '../../redux/actions/chatActions'
 
 export const Modal = () => {
     const modal = useSelector((state) => state.app.modal)
     const dispatch = useDispatch()
+    const history = useHistory()
     const modalRef = React.useRef()
+    const [input, setInput] = React.useState('')
+
     const handleCloseModal = () => {
         dispatch(hideModal())
     }
@@ -27,10 +33,14 @@ export const Modal = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    const handleBtnCreate = () => {
+        dispatch(createChat(input, history))
+    }
+
     if (!modal.visible) {
         return ''
     }
-
     return (
         <div className={styles.modalWrapper}>
             <div className={styles.modalBlock} ref={modalRef}>
@@ -41,8 +51,15 @@ export const Modal = () => {
                 <hr />
                 <p>{modal.text}</p>
                 <div className={styles.modalContent}>
-                    <input type="text" placeholder="Chat name" />
-                    <button type="submit">Create</button>
+                    <input
+                        type="text"
+                        placeholder="Chat name"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button type="submit" onClick={handleBtnCreate}>
+                        Create
+                    </button>
                 </div>
             </div>
         </div>
