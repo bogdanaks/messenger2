5a0cc6d4eeebe087fd9ff69cb1c6075861634888
bogdanaks <bogdanaks@bk.ru>
2020-09-14 14:35:38 +0300

    Refactoring code

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import styles from './styles.module.scss'
 
@@ -6,12 +7,20 @@ import { Sidebar } from '../../components/Sidebar/Sidebar'
 import { DialogsList } from '../../components/DialogsList/DialogsList'
 import { Dialog } from '../../components/Dialog/Dialog'
 
+import { initChat } from '../../redux/actions/chatActions'
+
 export const Main = () => {
+    const dispatch = useDispatch()
+    const chats = useSelector((state) => state.chat.chats)
+    React.useEffect(() => {
+        dispatch(initChat())
+        // eslint-disable-next-line
+    }, [])
     return (
         <>
             <Sidebar />
             <div className={styles.mainWrapper}>
-                <DialogsList />
+                <DialogsList chats={chats} />
                 <Dialog />
             </div>
         </>
