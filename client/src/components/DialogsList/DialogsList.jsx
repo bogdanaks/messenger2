ca9ae161b18a27a6e196e83eaa5b7661cf901563
bogdanaks <bogdanaks@bk.ru>
2020-09-14 12:08:17 +0300

    Add init chats dispatch and map

--- client/src/components/DialogsList/DialogsList.jsx
+++ client/src/components/DialogsList/DialogsList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import styles from './styles.module.scss'
 
 import { DialogsItem } from './DialogsItem'
+import { initChat } from '../../redux/actions/chatActions'
 
 export const DialogsList = () => {
+    const dispatch = useDispatch()
+    const chats = useSelector((state) => state.chat.chats)
+    React.useEffect(() => {
+        dispatch(initChat())
+        // eslint-disable-next-line
+    }, [])
     return (
         <div className={styles.dialogsWrapper}>
             <div className={styles.find}>
@@ -12,11 +20,9 @@ export const DialogsList = () => {
             </div>
             <div className={styles.dialogs}>
                 <ul>
-                    <DialogsItem />
-                    <DialogsItem active />
-                    <DialogsItem />
-                    <DialogsItem />
-                    <DialogsItem />
+                    {chats.map((item) => (
+                        <DialogsItem active key={item._id} {...item} />
+                    ))}
                 </ul>
             </div>
         </div>
