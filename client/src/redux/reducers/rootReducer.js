4334a34b8b4fa96d8f8944c107393466e8634bd3
bogdanaks <bogdanaks@bk.ru>
2020-09-14 11:20:47 +0300

    Import chat reducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import { appReducer } from './appReducer'
 import { userReducer } from './userReducer'
+import { chatReducer } from './chatReducer'
 
 export const rootReducer = combineReducers({
     app: appReducer,
     user: userReducer,
+    chat: chatReducer,
 })
