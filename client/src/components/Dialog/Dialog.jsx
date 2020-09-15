cb83bbdb6486ef9b77fd3e622a70b830649dcc7b
bogdanaks <bogdanaks@bk.ru>
2020-09-15 18:21:29 +0300

    Add btns settings chat

--- client/src/components/Dialog/Dialog.jsx
+++ client/src/components/Dialog/Dialog.jsx
@@ -3,14 +3,19 @@ import { useSelector } from 'react-redux'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGrin } from '@fortawesome/free-regular-svg-icons'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './styles.module.scss'
 
 import { Message } from '../Message/Message'
+import { ChatSettings } from '../ChatSettings/ChatSettings'
 
 export const Dialog = () => {
+    const [chatSettigs, setChatSettings] = React.useState(false)
     const activeChat = useSelector((state) => state.chat.activeChat)
+    const handleSettingsChat = () => {
+        setChatSettings(!chatSettigs)
+    }
     if (Object.keys(activeChat).length === 0) {
         return (
             <div className={styles.emptyChat}>
@@ -28,6 +33,12 @@ export const Dialog = () => {
                     <h5>{activeChat.name}</h5>
                     <span>{activeChat.users.length + ' members'}</span>
                 </div>
+                <div className={styles.chatSettings} onClick={handleSettingsChat}>
+                    <FontAwesomeIcon icon={faEllipsisV} />
+                </div>
+                {chatSettigs && (
+                    <ChatSettings chatId={activeChat._id} setChatSettings={setChatSettings} />
+                )}
             </div>
             <div className={styles.content}>
                 <div className={styles.content__messages}>
