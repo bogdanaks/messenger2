fe612f1fc7be58253874e33d50d7c552e279faff
bogdanaks <bogdanaks@bk.ru>
2020-09-15 18:20:15 +0300

    Add reduce for delete chat

--- client/src/redux/reducers/chatReducer.js
+++ client/src/redux/reducers/chatReducer.js
@@ -1,4 +1,4 @@
-import { INIT_CHAT, NEW_CHAT, SET_ACTIVE_CHAT } from '../actions/types'
+import { INIT_CHAT, NEW_CHAT, DELETE_CHAT, SET_ACTIVE_CHAT } from '../actions/types'
 
 const initialState = {
     chats: [],
@@ -11,6 +11,9 @@ export const chatReducer = (state = initialState, action) => {
             return { ...state, chats: action.payload }
         case NEW_CHAT:
             return { ...state, chats: state.chats.concat(action.payload) }
+        case DELETE_CHAT:
+            const res = state.chats.filter((chat) => chat._id !== action.payload)
+            return { ...state, chats: res }
         case SET_ACTIVE_CHAT:
             return { ...state, activeChat: action.payload }
         default:
