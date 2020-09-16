c4f4cfb8cb537281039d265db240fde5f256395f
bogdanaks <bogdanaks@bk.ru>
2020-09-16 12:33:19 +0300

    Add valudate form

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -7,6 +7,8 @@ import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 
 import styles from './styles.module.scss'
 
+import { useForm } from '../../utils/hooks/useForm'
+import { validateNewChat } from '../../utils/helpers/validateNewChat'
 import { hideModal } from '../../redux/actions/appActions'
 import { createChat } from '../../redux/actions/chatActions'
 
@@ -15,12 +17,15 @@ export const Modal = () => {
     const dispatch = useDispatch()
     const history = useHistory()
     const modalRef = React.useRef()
-    const [input, setInput] = React.useState('')
     const [colorNum, setColorNum] = React.useState(0)
     const colors = ['#7965c1', '#cb4f87', '#4388b9', '#00A876', '#d3a924']
+    const { errors, values, handleSubmit, handleChange } = useForm(callback, validateNewChat)
 
     const handleCloseModal = () => {
         dispatch(hideModal())
+    }
+    function callback() {
+        dispatch(createChat(values.newchat, colors[colorNum], history))
     }
     React.useEffect(() => {
         function handleClickOutside(event) {
@@ -36,9 +41,6 @@ export const Modal = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    const handleBtnCreate = () => {
-        dispatch(createChat(input, colors[colorNum], history))
-    }
     const handleColorClick = (index) => {
         setColorNum(index)
     }
@@ -53,32 +55,42 @@ export const Modal = () => {
                     <FontAwesomeIcon icon={faTimesCircle} />
                 </div>
                 <h3>{modal.header}</h3>
-                <hr />
+                <div className={styles.hr}></div>
                 <p>{modal.text}</p>
                 <div className={styles.modalContent}>
-                    <input
-                        type="text"
-                        placeholder="Chat name"
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                    />
-                    <div className={styles.colors}>
-                        <ul>
-                            {colors.map((color, index) => (
-                                <li
-                                    className={[
-                                        styles.color,
-                                        colorNum === index && styles.active,
-                                    ].join(' ')}
-                                    style={{ backgroundColor: color }}
-                                    onClick={() => handleColorClick(index)}
-                                    key={index}></li>
-                            ))}
-                        </ul>
-                    </div>
-                    <button type="submit" onClick={handleBtnCreate}>
-                        Create
-                    </button>
+                    <form onSubmit={handleSubmit}>
+                        <div
+                            className={[styles.inputForm, errors.newchat && styles.errorInput].join(
+                                ' ',
+                            )}>
+                            <input
+                                type="text"
+                                placeholder="Chat name"
+                                name="newchat"
+                                value={values.newchat || ''}
+                                onChange={handleChange}
+                            />
+                            {errors.newchat && (
+                                <span className={styles.errorText}>{errors.newchat}</span>
+                            )}
+                        </div>
+
+                        <div className={styles.colors}>
+                            <ul>
+                                {colors.map((color, index) => (
+                                    <li
+                                        className={[
+                                            styles.color,
+                                            colorNum === index && styles.active,
+                                        ].join(' ')}
+                                        style={{ backgroundColor: color }}
+                                        onClick={() => handleColorClick(index)}
+                                        key={index}></li>
+                                ))}
+                            </ul>
+                        </div>
+                        <button type="submit">Create</button>
+                    </form>
                 </div>
             </div>
         </div>
