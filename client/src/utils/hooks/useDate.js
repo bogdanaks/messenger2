558ef78b64cb3f7f0e1a1a9d7f9b904a2ccf1ba5
bogdanaks <bogdanaks@bk.ru>
2020-09-16 13:33:03 +0300

    Create hook useDate for formating date

---
+++ client/src/utils/hooks/useDate.js
@@ -0,0 +1,32 @@
+export const useDate = (date) => {
+    let dateFormat
+    let diffDays = Math.floor((Date.parse(new Date()) - Date.parse(date)) / 86400000) // Get different days betwen now days and updateDate
+    const addZero = (date) => {
+        return `0${date}`
+    }
+    const formateDateToFull = (nowDate) => {
+        let day = new Date(nowDate).getDate()
+        let month = new Date(nowDate).getMonth() + 1
+        let year = new Date(nowDate).getFullYear().toString().substr(-2)
+        if (day <= 9) {
+            day = addZero(day)
+        }
+        if (month <= 9) {
+            month = addZero(month)
+        }
+        return `${day}.${month}.${year}`
+    }
+    const formateDateToMinutes = (date) => {
+        return `${new Date(date).getHours()}:${new Date(date).getMinutes()}`
+    }
+
+    if (diffDays >= 1) {
+        // If date >= 1 day, return full date
+        dateFormat = formateDateToFull(date)
+    } else {
+        dateFormat = formateDateToMinutes(date)
+    }
+    return {
+        dateFormat,
+    }
+}
