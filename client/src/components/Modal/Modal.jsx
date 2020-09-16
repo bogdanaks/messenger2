537397b076f1b7b034874c146a96b8d555d463aa
bogdanaks <bogdanaks@bk.ru>
2020-09-16 15:44:05 +0300

    Code refactoring

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,32 +1,25 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 
 import styles from './styles.module.scss'
 
-import { useForm } from '../../utils/hooks/useForm'
-import { validateNewChat } from '../../utils/helpers/validateNewChat'
 import { hideModal } from '../../redux/actions/appActions'
-import { createChat } from '../../redux/actions/chatActions'
+
+import { NewChat } from './NewChat'
+import { InviteLink } from './InviteLink'
 
 export const Modal = () => {
     const modal = useSelector((state) => state.app.modal)
     const dispatch = useDispatch()
-    const history = useHistory()
     const modalRef = React.useRef()
-    const [colorNum, setColorNum] = React.useState(0)
-    const colors = ['#7965c1', '#cb4f87', '#4388b9', '#00A876', '#d3a924']
-    const { errors, values, handleSubmit, handleChange } = useForm(callback, validateNewChat)
 
     const handleCloseModal = () => {
         dispatch(hideModal())
     }
-    function callback() {
-        dispatch(createChat(values.newchat, colors[colorNum], history))
-    }
+
     React.useEffect(() => {
         function handleClickOutside(event) {
             if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -41,10 +34,6 @@ export const Modal = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    const handleColorClick = (index) => {
-        setColorNum(index)
-    }
-
     if (!modal.visible) {
         return ''
     }
@@ -57,41 +46,16 @@ export const Modal = () => {
                 <h3>{modal.header}</h3>
                 <div className={styles.hr}></div>
                 <p>{modal.text}</p>
-                <div className={styles.modalContent}>
-                    <form onSubmit={handleSubmit}>
-                        <div
-                            className={[styles.inputForm, errors.newchat && styles.errorInput].join(
-                                ' ',
-                            )}>
-                            <input
-                                type="text"
-                                placeholder="Chat name"
-                                name="newchat"
-                                value={values.newchat || ''}
-                                onChange={handleChange}
-                            />
-                            {errors.newchat && (
-                                <span className={styles.errorText}>{errors.newchat}</span>
-                            )}
-                        </div>
-
-                        <div className={styles.colors}>
-                            <ul>
-                                {colors.map((color, index) => (
-                                    <li
-                                        className={[
-                                            styles.color,
-                                            colorNum === index && styles.active,
-                                        ].join(' ')}
-                                        style={{ backgroundColor: color }}
-                                        onClick={() => handleColorClick(index)}
-                                        key={index}></li>
-                                ))}
-                            </ul>
-                        </div>
-                        <button type="submit">Create</button>
-                    </form>
-                </div>
+                {(() => {
+                    switch (modal.type) {
+                        case 'newChat':
+                            return <NewChat />
+                        case 'inviteLink':
+                            return <InviteLink />
+                        default:
+                            return ''
+                    }
+                })()}
             </div>
         </div>
     )
