1e09e9337e65ccb3f7636a81e6aa643f5e717e0c
bogdanaks <bogdanaks@bk.ru>
2020-09-16 12:33:40 +0300

    Fix validate

--- client/src/utils/helpers/validateNewChat.js
+++ client/src/utils/helpers/validateNewChat.js
@@ -1,16 +1,16 @@
 export const validateNewChat = (values) => {
     let errors = {}
-    if (!values) {
-        errors = 'Chat name is required'
+    if (!values.newchat) {
+        errors.newchat = 'Chat name is required'
     }
-    if (values && parseInt(values[0])) {
-        errors = 'The chat name must start with a letter, not a number'
+    if (values.newchat && parseInt(values.newchat[0])) {
+        errors.newchat = 'The chat name must start with a letter, not a number'
     }
-    if (values && values.length < 3) {
-        errors = 'Chat name at least 3 characters'
+    if (values.newchat && values.newchat.length < 3) {
+        errors.newchat = 'Chat name at least 3 characters'
     }
-    if (values && values.length >= 10) {
-        errors = 'Chat name up to 10 characters'
+    if (values.newchat && values.newchat.length >= 10) {
+        errors.newchat = 'Chat name up to 10 characters'
     }
     return errors
 }
