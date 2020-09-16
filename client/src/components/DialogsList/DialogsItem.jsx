25e35243b735da0569243463675d5db1d0a9ccb4
bogdanaks <bogdanaks@bk.ru>
2020-09-16 13:33:21 +0300

    Import and use hook useDate

--- client/src/components/DialogsList/DialogsItem.jsx
+++ client/src/components/DialogsList/DialogsItem.jsx
@@ -3,8 +3,11 @@ import { useParams } from 'react-router-dom'
 
 import styles from './styles.module.scss'
 
+import { useDate } from '../../utils/hooks/useDate'
+
 export const DialogsItem = ({ _id, name, updatedAt, color }) => {
     const { id } = useParams()
+    const { dateFormat } = useDate(updatedAt)
     return (
         <li className={id === _id ? styles.active : ''}>
             <div className={styles.chatImg}>
@@ -15,9 +18,7 @@ export const DialogsItem = ({ _id, name, updatedAt, color }) => {
             <div className={styles.chatInfo}>
                 <div className={styles.header}>
                     <h5>{name}</h5>
-                    <span>
-                        {new Date(updatedAt).getHours()}:{new Date(updatedAt).getMinutes()}
-                    </span>
+                    <span>{dateFormat}</span>
                 </div>
                 <p>Message last</p>
             </div>
