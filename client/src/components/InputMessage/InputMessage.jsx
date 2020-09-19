a7467780c02084a8c03bcc6e1df6cf8250ae637e
bogdanaks <bogdanaks@bk.ru>
2020-09-19 12:07:35 +0300

    Add useForm hook and dispatch message

--- client/src/components/InputMessage/InputMessage.jsx
+++ client/src/components/InputMessage/InputMessage.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGrin } from '@fortawesome/free-regular-svg-icons'
-
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import styles from './styles.module.scss'
 
-export const InputMessage = () => {
+import { useForm } from '../../utils/hooks/useForm'
+import { sendMessage } from '../../redux/actions/messageActions'
+
+export const InputMessage = ({ chat }) => {
+    const { values, handleChange, handleSubmit } = useForm(callback)
+    const dispatch = useDispatch()
+    function callback() {
+        dispatch(sendMessage(chat._id, values.message))
+    }
     return (
         <div className={styles.content__footer}>
-            <div className={styles.input}>
-                <input type="text" placeholder="Type text..." />
-            </div>
-            <div className={styles.icons}>
-                <FontAwesomeIcon icon={faGrin} />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.input}>
+                    <input
+                        type="text"
+                        placeholder="Type text..."
+                        autoComplete="off"
+                        name="message"
+                        value={values.message || ''}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className={styles.icons}>
+                    <FontAwesomeIcon icon={faGrin} />
+                    <button className={styles.iconSend} type="submit">
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
