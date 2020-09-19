d762a95029afce61d0169a2ca0114c45d2faeb29
bogdanaks <bogdanaks@bk.ru>
2020-09-19 13:44:18 +0300

    Dynamic output message

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import styles from './styles.module.scss'
 
-export const Message = ({ me }) => {
+import { useDate } from '../../utils/hooks/useDate'
+
+export const Message = ({ message }) => {
+    const { _id: userId } = JSON.parse(localStorage.getItem('user'))
+    const { dateFormat } = useDate(message.date)
     return (
-        <div className={[styles.msgWrapper, me && styles.me].join(' ')}>
+        <div className={[styles.msgWrapper, message.senderId === userId && styles.me].join(' ')}>
             <div className={styles.msgBlock}>
-                <h5 className={styles.name}>Name</h5>
-                <span className={styles.msg}>
-                    Как будут заканчивать, напиши, креветки закину! Окей, я понял тебя.
-                </span>
+                <h5 className={styles.name}>{message.senderName}</h5>
+                <span className={styles.msg}>{message.text}</span>
             </div>
-            <span className={styles.data}>18:55</span>
+            <span className={styles.data}>{dateFormat}</span>
         </div>
     )
 }
