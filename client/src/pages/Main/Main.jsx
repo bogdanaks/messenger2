5e32f14b0de99c31017346a918dcaa7ac06c1a3d
bogdanaks <bogdanaks@bk.ru>
2020-09-20 19:40:31 +0300

    Add socketGetMessages method

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -6,7 +6,12 @@ import { Sidebar } from '../../components/Sidebar/Sidebar'
 import { DialogsList } from '../../components/DialogsList/DialogsList'
 import { Dialog } from '../../components/Dialog/Dialog'
 
+import { socketGetMessages } from '../../utils/helpers/socket'
+
 export const Main = () => {
+    React.useEffect(() => {
+        socketGetMessages()
+    }, [])
     return (
         <>
             <Sidebar />
