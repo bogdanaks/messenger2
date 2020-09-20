a1cb7c52f06698e4bd515f1380b36929668b8f5e
bogdanaks <bogdanaks@bk.ru>
2020-09-20 19:39:59 +0300

    Create base sockets methods

--- server/server.js
+++ server/server.js
@@ -1,19 +1,44 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const http = require('http')
-require('dotenv').config()
+const socketio = require('socket.io')
+const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
+const publicPath = path.join(__dirname, '../client/public')
+const io = socketio(server)
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(publicPath))
 
 //routes
 app.use('/api/users', require('./routes/api/user.routes'))
 app.use('/api/chats', require('./routes/api/chat.routes'))
+
+//socket
+io.on('connection', (socket) => {
+    console.log('Socket connection')
+
+    socket.on('CHAT:JOIN', (data) => {
+        socket.join(data.chatId)
+    })
+    socket.on('CHAT:LEAVE', (data) => {
+        socket.leave(data.chatId)
+    })
+
+    socket.on('CHAT:SEND_MESSAGE', (data) => {
+        socket.to(data.inChatId).emit('CHAT:GET_MESSAGE', data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Socket disconnect')
+    })
+})
 
 //start server
 async function start() {
@@ -23,6 +48,7 @@ async function start() {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useCreateIndex: true,
+                useFindAndModify: false,
             })
             .then((res) => console.log(`Mongo start success`))
             .catch((err) => console.log(`Mongo error: ${err}`))
