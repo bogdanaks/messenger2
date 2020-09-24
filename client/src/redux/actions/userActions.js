f37f2ec9d9a0a0f18075ecf01fe25bd25625213f
bogdanaks <bogdanaks@bk.ru>
2020-09-24 13:34:35 +0300

    del get online method

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,7 +1,6 @@
-import { SET_USER, SET_USERS_IN_ACTIVE_CHAT } from './types'
+import { SET_USER } from './types'
 
 import api from '../../utils/helpers/axios'
-import { authHeader } from '../../utils/helpers/authHeader'
 import { showAlert, hideAlert } from './appActions'
 
 export function registerUser(name, password, history) {
@@ -36,26 +35,6 @@ export function loginUser(name, password, history) {
             )
             dispatch({ type: SET_USER, payload: { _id: res.data._id, name: res.data.name } })
             history.push('/')
-        } catch (error) {
-            if (error.response) {
-                dispatch(showAlert(error.response.status, error.response.data.message))
-                setTimeout(() => {
-                    dispatch(hideAlert())
-                }, 2000)
-            } else {
-                console.error(error)
-            }
-        }
-    }
-}
-
-export function getUsersInChat(chatId) {
-    return async (dispatch) => {
-        try {
-            const res = await api.get(`/api/chats/${chatId}/users/`, {
-                headers: authHeader(),
-            })
-            dispatch({ type: SET_USERS_IN_ACTIVE_CHAT, payload: res.data })
         } catch (error) {
             if (error.response) {
                 dispatch(showAlert(error.response.status, error.response.data.message))
