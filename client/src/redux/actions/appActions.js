4b3bd1fd8a5d1e2b9587589fbf777aae5ec3dae7
bogdanaks <bogdanaks@bk.ru>
2020-09-24 14:00:57 +0300

    add methods for hide and show dialog

--- client/src/redux/actions/appActions.js
+++ client/src/redux/actions/appActions.js
@@ -1,6 +1,5 @@
-import { SHOW_ALERT, HIDE_ALERT } from './types'
+import { SHOW_ALERT, HIDE_ALERT, HIDE_DIALOG, SHOW_DIALOG } from './types'
 
-// Alert actions
 export function showAlert(code, message) {
     return {
         type: SHOW_ALERT,
@@ -9,4 +8,11 @@ export function showAlert(code, message) {
 }
 export function hideAlert() {
     return { type: HIDE_ALERT }
+}
+
+export function hideDialog() {
+    return { type: HIDE_DIALOG }
+}
+export function showDialog() {
+    return { type: SHOW_DIALOG }
 }
